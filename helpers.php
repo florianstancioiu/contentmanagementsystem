c3322966ed68cfe7b72080137d6ad430fb54dcd7
Florian Stancioiu <s.florian93@gmail.com>
2019-07-24 02:16:03 +0300

    Create a bunch of helper functions

--- helpers.php
+++ helpers.php
@@ -3,12 +3,20 @@
 use Twig\Environment;
 use Twig\Loader\FilesystemLoader;
 
-/**
- * @param string $template
- * @param array $data
- */
+if (! defined('DS')) {
+    define('DS', DIRECTORY_SEPARATOR);
+}
+
 if (! function_exists('view')) {
-    function view(string $template, array $data) {
+    /**
+     * @param string $template
+     * @param array $data
+     * @return string
+     * @throws \Twig\Error\LoaderError
+     * @throws \Twig\Error\RuntimeError
+     * @throws \Twig\Error\SyntaxError
+     */
+    function view(string $template, array $data) : string {
         // Load Twig templates
         $loader = new Twig\Loader\FilesystemLoader('./resources/views');
 
@@ -19,5 +27,46 @@ if (! function_exists('view')) {
         $template = $twig->load($template);
 
         echo $template->render($data);
+    }
+}
+
+if (! function_exists('read_json_file')) {
+    /**
+     * @param string $file_path
+     * @return array
+     */
+    function read_json_file(string $file_path) : array {
+        try {
+            $contents = file_get_contents($file_path);
+            $array = json_decode($contents, true);
+        } catch (Throwable $e) {
+            return [];
+        }
+
+        return is_null($array) ? [] : $array;
+    }
+}
+
+if (! function_exists('base_dir')) {
+    /**
+     * @return string
+     */
+    function base_dir() : string {
+        return dirname(getcwd());
+    }
+}
+
+if (! function_exists('dd')) {
+    /**
+     * @param mixed ...$data
+     */
+    function dd(... $data) {
+        echo '<pre>';
+        foreach ($data as $entry) {
+            print_r($entry);
+            echo '<br/>';
+        }
+        echo '</pre>';
+        exit();
     }
 }
