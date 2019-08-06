fa9c4a57e44db40575a852b1f365e6fee33fb1f0
Florian Stancioiu <s.florian93@gmail.com>
2019-08-06 10:47:43 +0300

    Update .gitignore
    Update webpack.mix.js

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,7 @@
 const mix = require('laravel-mix');
 
 mix
-    .js('resources/js/app.js', 'public/js')
-    .sass('resources/scss/app.scss', 'public/css');
+    .js('resources/js/admin.js', 'public/js')
+    .js('resources/js/client.js', 'public/js')
+    .sass('resources/scss/client.scss', 'public/css')
+    .sass('resources/scss/admin.scss', 'public/css');
