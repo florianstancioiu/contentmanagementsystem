de766c7a8f6693d5288de4d94f6b6618ea2bb405
Florin Stancioiu <florinvictorstancioiu@gmail.com>
2019-09-09 19:47:40 +0300

    Small .gitignore and HomeController update

--- client/Controllers/HomeController.php
+++ client/Controllers/HomeController.php
@@ -6,10 +6,10 @@ use Common\Controller;
 
 class HomeController extends Controller
 {
-		public function homepage()
-		{
-				$user = 'stuff';
+    public function homepage()
+    {
+        $user = 'stuff';
 
-				return view('client/home/show', compact('user'));
-		}
+        return view('client/common/home', compact('user'));
+    }
 }
