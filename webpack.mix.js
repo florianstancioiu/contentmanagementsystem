843a522d324158f6577a27576ada40613d23d934
Florin Stancioiu <florinvictorstancioiu@gmail.com>
2019-09-09 15:53:34 +0300

    Update layout twig files

--- webpack.mix.js
+++ webpack.mix.js
@@ -4,4 +4,5 @@ mix
     .js('resources/js/admin.js', 'public/js')
     .js('resources/js/client.js', 'public/js')
     .sass('resources/scss/client.scss', 'public/css')
-    .sass('resources/scss/admin.scss', 'public/css');
+    .sass('resources/scss/admin.scss', 'public/css')
+    .version();
